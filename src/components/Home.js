fe40d26fde8d1ac2b6aaf2cfce075d08b75b07f3
Doug Wright <dw@nof49.ca>
2021-01-12 14:36:03 -0500

    Add home page animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import imgCookieMonster from '../cookie-monster.png';
+
 const Home = () => {
 
   return (
     <Wrapper>
       <Title>Cookie game</Title>
       <Link to="/game">Go to game</Link>
+      <Img src={imgCookieMonster} />
     </Wrapper>
   );
 };
@@ -22,6 +25,21 @@ const Wrapper = styled.div`
 const Title = styled.h1`
   font-size: 48px;
   margin-bottom: 32px;
+`;
+
+const Img = styled.img`
+  margin-top: 50px;
+  animation: scaleIt 1s alternate infinite ease-in-out;
+
+  @keyframes scaleIt {
+    0% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(1.1);
+    }
+  }
+}
 `;
 
 export default Home;
